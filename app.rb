64422f877bccdc214d0dc32431d7907c08f2d2ff
Benjamin Cheng <bencheng200@gmail.com>
2021-01-25 23:04:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+<<<<<<< HEAD
 
   configure do
     enable :sessions
     set :session_secret, 'flatiron'
+=======
+  configure do
+    enable :sessions
+    set :session_secret, "secretcookie"
+>>>>>>> b38c254254c2411d4fd9492909c51a65e08a5988
   end
 
   get '/' do
@@ -12,8 +18,15 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
+<<<<<<< HEAD
     session[:item] = params[:item]
     @session = session
+=======
+    @sessions = session
+    item = params["item"]
+    @sesions[:item] = item
+
+>>>>>>> b38c254254c2411d4fd9492909c51a65e08a5988
     erb :checkout
   end
 end
